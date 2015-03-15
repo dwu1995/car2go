2c24c5b92a1a4d487c48450b0ec5c438f7588e1b
jchan110 <jerrychan37@gmail.com>
2015-03-14 22:19:09 -0700

    userSettings, settings(xml), strings(xml)

--- mobile/src/main/java/co2go/myapplication/userSettings.java
+++ mobile/src/main/java/co2go/myapplication/userSettings.java
@@ -4,21 +4,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class userSettings extends ActionBarActivity {
+public class userSettings extends ActionBarActivity implements View.OnClickListener {
+    Button nameButton;
+    Button manufacturerButton;
+    Button modelButton;
+
+    EditText nameSet;
+    EditText manufacturerSet;
+    EditText modelSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
-    }
 
+        nameButton = (Button) findViewById(R.id.button_name);
+        nameButton.setOnClickListener(this);
+        manufacturerButton = (Button) findViewById(R.id.button_name2);
+        manufacturerButton.setOnClickListener(this);
+        modelButton = (Button) findViewById(R.id.button_name3);
+        modelButton.setOnClickListener(this);
+        nameSet = (EditText) findViewById(R.id.userName);
+        manufacturerSet = (EditText) findViewById(R.id.car_manufacturer);
+        modelSet = (EditText) findViewById(R.id.car_model);
+        nameSet.setTag(0);
+        manufacturerSet.setTag(1);
+        modelSet.setTag(2);
+
+    }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_user_settings, menu);
         return true;
     }
@@ -36,5 +61,27 @@ public class userSettings extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = new String();
+        switch (v.getId()) {
+            case R.id.button_name:
+                nameSet.setText("hello " + nameSet.getText());
+                text = nameSet.getText().toString();
+                break;
+            case R.id.button_name2:
+                manufacturerSet.setText("hello " + manufacturerSet.getText());
+                text = manufacturerSet.getText().toString();
+                break;
+            case R.id.button_name3:
+                modelSet.setText("hello " + modelSet.getText());
+                text = modelSet.getText().toString();
+                break;
+            default:
+                break;
+        }
+
     }
 }
